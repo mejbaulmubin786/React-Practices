91ae9dbc7d5969095973022f419239199e322cf8
Mejbaul Mubin <mejbaulmubin@my.uopeople.edu>
2024-09-04 17:13:35 +0600

    Brize install and some database

---
+++ database/migrations/2024_09_04_110614_create_exchanges_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void {
+        Schema::create('exchanges', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('requester_id');
+            $table->unsignedBigInteger('provider_id');
+            $table->string('requested_skill');
+            $table->string('provided_skill');
+            $table->text('details');
+            $table->timestamps();
+
+            $table->foreign('requester_id')->references('id')->on('users')->onDelete('cascade');
+            $table->foreign('provider_id')->references('id')->on('users')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void {
+        Schema::dropIfExists('exchanges');
+    }
+};
