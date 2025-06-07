e9b5bac33d920693ab8ccdae8501f35074bc6196
mejbaulmubin786 <mejbaul_mubin@yahoo.com>
2025-06-07 22:24:45 +0600

    update

--- project1/project1/src/App.jsx
+++ project1/project1/src/App.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import Title from './components/Title'
+import Card from './components/Card'
 
 const App = () => {
   return (
     <>
-      <Title />
+      <Card design="design-one" title="Card-1" text="Some quick example text to build on the card title and make up the bulk of the card’s content."/>
+      <Card />
+      <Card />
+      <Card />
     </>
   )
 }
