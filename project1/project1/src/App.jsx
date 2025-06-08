643c6b323c21b6101c35dead7421218137ef15fd
mejbaulmubin786 <mejbaul_mubin@yahoo.com>
2025-06-08 13:36:18 +0600

    update

--- project1/project1/src/App.jsx
+++ project1/project1/src/App.jsx
@@ -5,9 +5,9 @@ import Card from './components/Card'
 const App = () => {
   return (
     <>
-      <Card design="design-one" title="Card-1" text="Some quick example text to build on the card title and make up the bulk of the card’s content."/>
-      <Card />
-      <Card />
+      <Card design="design-one" title="Card-1" text="Some quick example text to build on the card title and make up the bulk of the card’s content." type={true}/>
+      <Card design="design-two" title="Card-2" text="Some quick example text to build on the card title and make up the bulk of the card’s content." type={false}/>
+      <Card design="design-one" title="Card-3" text="Some quick example text to build on the card title and make up the bulk of the card’s content." type={true}/>
       <Card />
     </>
   )
--- project1/project1/src/components/Card.jsx
+++ project1/project1/src/components/Card.jsx
@@ -4,15 +4,15 @@ const Card = ({title, text, design, type}) => {
   
   return (
     <>
-      <div className={design} style={{ width: '18rem' }}> {/* এখানে className এবং style ঠিক করা হয়েছে */}
+      <div className={`card ${design}`} style={{ width: '18rem' }}> {/* এখানে className এবং style ঠিক করা হয়েছে */}
         <img src="..." className="card-img-top" alt="..." /> {/* img ট্যাগে self-closing স্ল্যাশ যোগ করা হয়েছে */}
         <div className="card-body">
           <h5 className="card-title">{title}</h5>
           <p className="card-text">{text}</p>
           {
-            type ? 
+            type ? <a href="#" className="btn btn-primary">Go somewhere</a> : "" 
           }
-          <a href="#" className="btn btn-primary">Go somewhere</a>
+          
         </div>
       </div>
     </>
