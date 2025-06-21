6e781151bfa584ebf37e16835198c40bd4a09cf0
mejbaulmubin786 <mejbaul_mubin@yahoo.com>
2025-06-21 11:14:32 +0600

    remove old file

--- Old/html-and-css/CreativeIt/OLd-Work/New_fol/topbar-navigationbar/script.js
+++
@@ -1,9 +0,0 @@
-// script.js
-window.addEventListener("scroll", function () {
-  const navBar = document.getElementById("navBar");
-  if (window.scrollY > 100) {
-    navBar.classList.add("scrolled");
-  } else {
-    navBar.classList.remove("scrolled");
-  }
-});
