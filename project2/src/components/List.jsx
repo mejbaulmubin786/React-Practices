cdae11d0870211974213a90cbfe1c21c70d3fc9c
mejbaulmubin786 <mejbaul_mubin@yahoo.com>
2025-06-30 20:46:52 +0600

    update font

--- project2/src/components/List.jsx
+++ project2/src/components/List.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const List = ({text}) => {
   return (
     <>
-    <li className='text-[#EFF4FA] text-lg font-semibold'>{text}</li>
+    <li className='text-[#EFF4FA] text-lg font-semibold font-open'>{text}</li>
     </>
   )
 }
