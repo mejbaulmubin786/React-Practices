bbdd2b60706d859a45c3645d7349744294bfe059
mejbaulmubin786 <mejbaul_mubin@yahoo.com>
2025-06-30 22:17:12 +0600

    create container

--- project2/src/App.jsx
+++ project2/src/App.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import List from './components/List'
 import Button from './components/Button'
-
+import Image from './components/Image'
+import Container from './components/Container'
 const App = () => {
   return (
     <>
+      <Container>
+        <Image />
       <List text = "Home"/> 
       <List text = "About" className=""/>
       <List text = "Service" className=""/>
@@ -12,6 +15,7 @@ const App = () => {
       <List text = "Price" className=""/>
       <List text = "Blog" className=""/>
       <Button text="Contact Us" />
+      </Container>
     </>
   )
 }
---
+++ project2/src/components/Image.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Banner from '../assets/banner.png'
+
+const Image = () => {
+  return (
+    <>
+      <img src={Banner} />
+    </>
+  )
+}
+
+export default Image
