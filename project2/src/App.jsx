12149d8126f8aee5cf0c9f9ef74fcdb24690d590
mejbaulmubin786 <mejbaul_mubin@yahoo.com>
2025-07-01 17:07:47 +0600

    update navbar component

--- project2/src/App.jsx
+++ project2/src/App.jsx
@@ -1,24 +1,11 @@
 import React from 'react'
-import List from './components/List'
-import Button from './components/Button'
-import Image from './components/Image'
-import Container from './components/Container'
-import Flex from './components/Flex'
+
+import Navbar from './layouts/Navbar'
+
 const App = () => {
   return (
     <>
-      <Container>
-        <Flex className='justify-center items-center gap-x-[42.5px]'>
-        <Image />
-      <List text = "Home"/> 
-      <List text = "About" className=""/>
-      <List text = "Service" className=""/>
-      <List text = "Portfolio" className=""/>
-      <List text = "Price" className=""/>
-      <List text = "Blog" className=""/>
-      <Button text="Contact Us" />
-      </Flex>
-      </Container>
+      <Navbar />
     </>
   )
 }
---
+++ project2/src/layouts/Navbar.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import List from '../components/List'
+import Button from '../components/Button'
+import Image from '../components/Image'
+import Container from '../components/Container'
+import Flex from '../components/Flex'
+
+const Navbar = () => {
+  return (
+    <Container>
+        <Flex className='justify-center items-center gap-x-[42.5px]'>
+        <Image />
+      <List text = "Home"/> 
+      <List text = "About" className=""/>
+      <List text = "Service" className=""/>
+      <List text = "Portfolio" className=""/>
+      <List text = "Price" className=""/>
+      <List text = "Blog" className=""/>
+      <Button text="Contact Us" />
+      </Flex>
+      </Container>
+  )
+}
+
+export default Navbar
