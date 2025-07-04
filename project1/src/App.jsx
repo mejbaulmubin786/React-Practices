9d657263ff6809fcb6b85a4945660963954515bf
Mejbaul Mubin <mejbaul_mubin@yahoo.com>
2025-07-05 01:36:12 +0600

    update service section

--- project1/src/App.jsx
+++ project1/src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Navber from './Layouts/Navber'
 import Banner from './layouts/Banner'
+import Service from './Layouts/Service'
 
 const App = () => {
   return (
     <div>
       <Navber />
       <Banner />
+      <Service />
     </div>
   )
 }
